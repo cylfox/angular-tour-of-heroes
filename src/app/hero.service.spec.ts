2ab5a0980855c9cb777f561339ece31ea7d12c9f
Marcos Sepúlveda Romero <msepulvr@everis.com>
2021-04-13 13:26:26 +0200

    Added tests to message service

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,12 +1,17 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { HeroService } from './hero.service';
 
 describe('HeroService', () => {
   let service: HeroService;
+  // let httpClientSpy: {get: jasmine.Spy}
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    // constructor(private messageService: MessageService, private http: HttpClient) { }
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler]
+    });
     service = TestBed.inject(HeroService);
   });
 
--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -6,6 +6,7 @@ describe('MessageService', () => {
   let service: MessageService;
 
   beforeEach(() => {
+    // The TestBed creates a dynamically-constructed Angular test module that emulates an Angular @NgModule
     TestBed.configureTestingModule({});
     service = TestBed.inject(MessageService);
   });
@@ -13,4 +14,19 @@ describe('MessageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add a message', () => {
+    service.add('test message');
+    expect(service.messages.length).toBe(1);
+    expect(service.messages[0]).toBe('test message');
+  });
+
+  it('should clear all messages', () => {
+    service.add('test message 1');
+    service.add('test message 2');
+    expect(service.messages.length).toBe(2);
+    service.clear();
+    expect(service.messages.length).toBe(0);
+  });
+
 });
