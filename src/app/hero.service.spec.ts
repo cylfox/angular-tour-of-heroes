dfdc9a7aa8b51a8151592e0f985398b42457ddc7
Marcos Sepúlveda Romero <msepulvr@everis.com>
2021-04-13 15:28:04 +0200

    Added http get tests to hero service

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,21 +1,50 @@
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { Observable, of, throwError } from 'rxjs';
+import { Hero } from './hero';
 
 import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
 
 describe('HeroService', () => {
-  let service: HeroService;
-  // let httpClientSpy: {get: jasmine.Spy}
+  let heroService: HeroService;
+  let httpClientSpy: { get: jasmine.Spy }
 
   beforeEach(() => {
     // constructor(private messageService: MessageService, private http: HttpClient) { }
-    TestBed.configureTestingModule({
-      providers: [HttpClient, HttpHandler]
-    });
-    service = TestBed.inject(HeroService);
+    // TestBed.configureTestingModule({
+    //   providers: [HttpClient, HttpHandler]
+    // });
+    // service = TestBed.inject(HeroService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    heroService = new HeroService(new MessageService, httpClientSpy as any);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(heroService).toBeTruthy();
   });
+
+  it('should return expected heroes', () => {
+    const expectedHeroes: Hero[] =
+      [{ id: 1, name: 'A' }, { id: 2, name: 'B' }];
+
+    // prepare the http get response
+    httpClientSpy.get.and.returnValue(of(expectedHeroes));
+
+    heroService.getHeroes().subscribe(heroes =>
+      expect(heroes).toEqual(expectedHeroes, 'expected heroes array'),
+      fail
+    )
+  })
+
+  it('should return an empty array when the server returns an error', () => {
+    const expectedHeroes: Hero[] = [];
+    
+    httpClientSpy.get.and.returnValue(throwError({status: 404}));
+
+    heroService.getHeroes().subscribe(heroes =>
+      expect(heroes).toEqual(expectedHeroes, 'expected heroes array'),
+      fail
+    )
+  })
 });
